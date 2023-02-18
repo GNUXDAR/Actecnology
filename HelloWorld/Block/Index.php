3c71dba198b1042fdbb5e79b4f0782fa2eeacbe2
Arturo Cabrera <gnuxdar@gmail.com>
2023-02-17 23:40:34 -0500

    [UPD] changed link

--- HelloWorld/Block/Index.php
+++ HelloWorld/Block/Index.php
@@ -1,5 +1,6 @@
 <?php
-namespace Actecnology\HelloWorld\Block;
+
+namespace Actecnology\HelloWorld\Block\Request;
 class Index extends \Magento\Framework\View\Element\Template
 {
     public function __construct(\Magento\Framework\View\Element\Template\Context $context)
@@ -9,6 +10,6 @@ class Index extends \Magento\Framework\View\Element\Template
  
     public function sayHello()
     {
-        return __('<h2>Bienvenidos al Actecnologies.com</h2>');
+        return __('<h2>Bienvenidos al sitio de  Actecnologies.com</h2>');
     }
 }

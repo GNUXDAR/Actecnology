17bb778aec4016c57cbc6f30aa8a444be27100a6
Arturo Cabrera <gnuxdar@gmail.com>
2023-02-20 18:54:16 -0500

    [UPD]

--- ModuloBasico/Block/Adminhtml/Subscription.php
+++ ModuloBasico/Block/Adminhtml/Subscription.php
@@ -8,10 +8,11 @@ class Subscription extends \Magento\Backend\Block\Widget\Grid\Container
 {
     protected function _construct()
     {
-        $this->_controller = 'adminhtml_subscription';
         $this->_blockGroup = 'Actecnology_ModuloBasico';
-        $this->_headerText = __('Elemento Gnuxdar');
-        $this->_addButtonLabel = __('Add News');
+        $this->_controller = 'adminhtml_subscription';
+
+        // $this->_headerText = __('Elemento Gnuxdar');
+        // $this->_addButtonLabel = __('Add News');
         parent::_construct();
     }
 }
--- ModuloBasico/Controller/Index/Subscription.php
+++ ModuloBasico/Controller/Index/Subscription.php
@@ -8,12 +8,14 @@ class Subscription extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
+        // el objectManager no se recomeinda utilizar directamente, se prefiere utilizar una injeccion de dependencia
+        // para ejecutar: URL: /frontname_de_router/controlador_folder/class_file = {url}/actecnology/index/subscription/
         $subscription = $this->_objectManager->create('Actecnology\ModuloBasico\Model\Subscription');
 
         $subscription->setFirstname('Arturo');
-        $subscription->setLastname('Cabrtera');
+        $subscription->setLastname('Cabrera');
         $subscription->setEmail('gnuxdar@gmail.com');
-        $subscription->setMessage('Mensaje registro');
+        $subscription->setMessage('Mensaje de registro desde el controller');
 
         $subscription->save();
         $this->getResponse()->setBody('Success');

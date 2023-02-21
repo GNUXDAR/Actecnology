929cc6f6db7ee4b539d8bc45dfaba94710c0efb2
Arturo Cabrera <gnuxdar@gmail.com>
2023-02-20 20:42:02 -0500

    [UPD] new messsage to item in grid admin

--- ModuloBasico/Controller/Index/Subscription.php
+++ ModuloBasico/Controller/Index/Subscription.php
@@ -12,10 +12,11 @@ class Subscription extends \Magento\Framework\App\Action\Action
         // para ejecutar: URL: /frontname_de_router/controlador_folder/class_file = {url}/actecnology/index/subscription/
         $subscription = $this->_objectManager->create('Actecnology\ModuloBasico\Model\Subscription');
 
-        $subscription->setFirstname('Arturo');
+        $subscription->setFirstname('Alexander');
         $subscription->setLastname('Cabrera');
-        $subscription->setEmail('gnuxdar@gmail.com');
-        $subscription->setMessage('Mensaje de registro desde el controller');
+        $subscription->setEmail('newbie@gmail.com');
+        $subscription->setMessage('Tercero mensaje de registro desde el controller');
+        // $subscription->setStatus('approved');
 
         $subscription->save();
         $this->getResponse()->setBody('Success');

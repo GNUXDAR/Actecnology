ec11bc47f227cdb4a7895a23aae4257cd285d70f
Arturo Cabrera <gnuxdar@gmail.com>
2023-02-22 18:21:04 -0500

    [NEW]Block to Button SAve and Back

---
+++ ModuloBasico/Block/Adminhtml/Subscription/Edit/Button/Save.php
@@ -0,0 +1,86 @@
+
+<?php
+
+namespace Actecnology\ModuloBasico\Block\Adminhtml\Subscription\Edit\Button;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Magento\Ui\Component\Control\Container;
+use Magento\Catalog\Block\Adminhtml\Product\Edit\Button\Generic;
+
+class Save extends Generic implements ButtonProviderInterface
+{
+    /**
+     * Get button data
+     *
+     * @return array
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Guardar'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => [
+                    'buttonAdapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'modulobasico_form.modulobasico_form',
+                                'actionName' => 'save',
+                                'params' => [false],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'class_name' => Container::SPLIT_BUTTON,
+            'options' => $this->getOptions(),
+        ];
+    }
+    /**
+     * Retrieve options
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        $options[] = [
+            'id_hard' => 'save_and_new',
+            'label' => __('Guardar Nuevo'),
+            'data_attribute' => [
+                'mage-init' => [
+                    'buttonAdapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'modulobasico_form.modulobasico_form',
+                                'actionName' => 'save',
+                                'params' => [
+                                    true,[
+                                        'back' => 'add',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $options[] = [
+            'id_hard' => 'save_and_close',
+            'label' => __('Guardar y Cerrar'),
+            'data_attribute' => [
+                'mage-init' => [
+                    'buttonAdapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'modulobasico_form.modulobasico_form',
+                                'actionName' => 'save',
+                                'params' => [true],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        return $options;
+    }
+}

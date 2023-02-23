cc4839346c8b9525b7b33d6706542e21eb9cc1ab
Arturo Cabrera <gnuxdar@gmail.com>
2023-02-22 21:11:24 -0500

    [NEW] Controller Add.php

---
+++ ModuloBasico/Controller/Adminhtml/Subscription/Add.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Actecnology\ModuloBasico\Controller\Adminhtml\Subscription;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Add extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param \Actecnology\ModuloBasico\Model\Subscription $subscriptionFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ){
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Agregar Nuevo Registro'));
+        return $resultPage;
+    }
+}

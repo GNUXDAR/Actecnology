fe61d073384a98b2e31c3bf4ffdfd0ae8d052cbc
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-06-18 11:18:27 -0500

    [UPD] to show template in front

--- GetProvider/Block/Offer.php
+++ GetProvider/Block/Offer.php
@@ -7,39 +7,10 @@ use Actecnology\GetProvider\Model\ProviderAPI;
 
 class Offer extends Template
 {
-    protected $providerAPI;
-
     public function __construct(
-        Template\Context $context,
-        ProviderAPI $providerAPI,
+        \Magento\Framework\View\Element\Template\Context $context,  //para inyeccion de dependencias
         array $data = []
     ) {
-        $this->providerAPI = $providerAPI;
         parent::__construct($context, $data);
     }
-
-    public function getBestOffer($sku)
-    {
-        $offers = $this->providerAPI->getAllSkuOffers($sku);
-
-        // Obtener todas las ofertas del proveedor
-        $allOffers = $this->providerAPI->getAllOffers($sku);
-
-        // Variables para almacenar la mejor oferta
-        $bestOfferPrice = null;
-        $bestOffer = null;
-
-        foreach ($allOffers as $offer) {
-            // Verificar si la oferta es la mejor hasta el momento
-            if ($bestOfferPrice === null || $offer['price'] < $bestOfferPrice) {
-                $bestOfferPrice = $offer['price'];
-                $bestOffer = $offer;
-            }
-        }
-        // se puede utilizar los datos de $offers para comparar precios, disponibilidad, calificaciones, etc.
-
-        // Devuelve la mejor oferta encontrada
-        return $bestOffer;
-    }
-
-    }
+}

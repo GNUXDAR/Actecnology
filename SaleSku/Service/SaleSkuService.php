4d895c49cefefecf0913edcedd229dc8c1015a87
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-06-19 23:12:40 -0500

    [UPD] add new method saveDailySalesReport

--- SaleSku/Service/SaleSkuService.php
+++ SaleSku/Service/SaleSkuService.php
@@ -6,6 +6,9 @@ use Actecnology\SaleSku\Api\SaleSkuServiceInterface;
 use Actecnology\SaleSku\Api\Data\SaleInterface;
 use Actecnology\SaleSku\Api\Data\SaleInterfaceFactory;
 
+use Actecnology\SaleSku\Model\SaleFactory;
+use Actecnology\SaleSku\Model\DailySalesReportFactory;
+
 use Actecnology\SaleSku\Model\Sale; //test
 
 class SaleSkuService implements SaleSkuServiceInterface
@@ -13,20 +16,28 @@ class SaleSkuService implements SaleSkuServiceInterface
     protected $saleInterfaceFactory;
     protected $saleModel;
 
+    protected $saleFactory;
+    protected $dailySalesReportFactory;
+
     public function __construct(
         SaleInterfaceFactory $saleInterfaceFactory,
-        Sale $saleModel
+        Sale $saleModel,
+        SaleFactory $saleFactory,
+        DailySalesReportFactory $dailySalesReportFactory
     ) {
         $this->saleInterfaceFactory = $saleInterfaceFactory;
         $this->saleModel = $saleModel;
+        $this->saleFactory = $saleFactory;
+        $this->dailySalesReportFactory = $dailySalesReportFactory;
     }
 
-    public function registerSale($sku, $offerId, $orderId)
+    public function registerSale($sku, $offerId, $orderId, $quantity)
     {
-        $sale = $this->saleInterfaceFactory->create();
+        $sale = $this->saleFactory->create();
         $sale->setSku($sku);
         $sale->setOfferId($offerId);
         $sale->setOrderId($orderId);
+        $sale->setQuantity($quantity);
         $sale->save();
 
         return $sale;
@@ -34,8 +45,7 @@ class SaleSkuService implements SaleSkuServiceInterface
 
     public function getSalesBySku($sku)
     {
-        // Logic to retrieve sales by SKU from your data source
-        $sales = []; // Placeholder
+        $sales = [];
 
         return $sales;
     }
@@ -52,9 +62,44 @@ class SaleSkuService implements SaleSkuServiceInterface
 
     public function getSalesInformation()
     {
-        // Logic to retrieve all sales information from your data source
-        $sales = []; // Placeholder
+        $sales = [];
 
         return $sales;
+    }
+
+    // - - -
+
+    public function getSKUs()
+    {
+        // obtener los SKUs únicos que se han vendidos
+        $sales = $this->saleFactory->create()->getCollection();
+        $sales->getSelect()->reset(\Zend_Db_Select::COLUMNS)->columns('DISTINCT(sku)');
+
+        $skus = [];
+        foreach ($sales as $sale) {
+            $skus[] = $sale->getSku();
+        }
+
+        return $skus;
+    }
+
+    public function getSalesBySkuAndDate($sku, $date)
+    {
+        // Obtener las ventas por SKU y fecha que se creo
+        $sales = $this->saleFactory->create()->getCollection();
+        $sales->addFieldToFilter('sku', $sku);
+        $sales->addFieldToFilter('created_at', ['like' => $date . '%']);
+
+        return $sales;
+    }
+
+    public function saveDailySalesReport($date, $sku, $totalSales)
+    {
+        // Guardar la información en el informe diario de ventas
+        $dailySalesReport = $this->dailySalesReportFactory->create();
+        $dailySalesReport->setDate($date);
+        $dailySalesReport->setSku($sku);
+        $dailySalesReport->setTotalSales($totalSales);
+        $dailySalesReport->save();
     }
 }

8b4d5f2ecd3eb9718cd9ac7cde57b74acc2ec0cf
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-07-18 19:09:39 -0500

    [NEW] module AdminMenu core module to admin

---
+++ AdminMenu/registration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author AC Tecnology Team (https://actecnology.com/ ó https://arturocabrera.com/)
+ * Copyright ©  All rights reserved.
+ * Dev: Arturo Cabrera
+ * mail: arturo.actecnology@gmail.com
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Actecnology_AdminMenu',
+    __DIR__
+);
